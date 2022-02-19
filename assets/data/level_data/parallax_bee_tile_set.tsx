87afadd11476798aacfea78ff2f22e650b0f7480
blankRiot96 <veresharjun5@gmail.com>
2022-02-19 18:05:18 +0530

    if you dont fucking work-

---
+++ assets/data/level_data/parallax_bee_tile_set.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="parallax_bee_tile_set" tilewidth="25" tileheight="25" tilecount="9" columns="3">
+ <image source="../../sprites/tilesets/parallax_bee_tile_set.png" width="75" height="75"/>
+</tileset>
